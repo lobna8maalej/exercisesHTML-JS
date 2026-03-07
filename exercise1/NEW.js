32530b00e4bbece35b4be0667c0bed9905b7a7a2
lobna8lobna <lobna89@gmail.com>
2026-03-07 21:22:36 +0100

    envoyer l'essai

--- exercise1/NEW.js
+++ exercise1/NEW.js
@@ -1,47 +1,71 @@
 
-let page = document.getElementById("page");
-let image = document.getElementById("image");
-let message = document.getElementById("message");
+var theme = prompt("choisir un theme: green, red, blue");
+var img = document.getElementById(img);
+var message = document.getElementById("theme message");
 
+if (theme === "green") {
+    style.backgroundColor = "#c8e6c9";
+    style.img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb66GL5N3iqA9fKJW4k0ELFuwaBMUp1bGXmg&s";
+    message.textContent = "theme vert applique";
+}
+if (theme === "blue") {
+    theme.style.backgroundColor = "blue";
+    theme.style.color = darkblue;
+    img.src = "https://upload.wikimedia.org/wikipedia/commons/5/5e/Blue_ocean.jpg";
+    message.textContent = "theme blue applique";
+}
+if (theme === "red") {
+    theme.style.border = "red";
+    img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA2oASENe7GdCli1KnAl6cnDnuD6lGT60txQ&s";
+    message.textContent = "theme red applique";
+}
 
-let theme;
 while (true) {
-    theme = prompt("Choisir un thème : green, red, blue");
-    if (theme === "green" || theme === "red" || theme === "blue") {
-        break;
+    let theme = prompt("Choisir un thème : green, red, blue"); 
+
+    if (theme === "green") {
+        style.backgroundColor = "#c8e6c9";
+        style.img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb66GL5N3iqA9fKJW4k0ELFuwaBMUp1bGXmg&s";
+        message.textContent = "theme vert applique";
     }
-    alert("Invalid input. Please type green, blue, or red");
-}
-
-
-if (theme === "green") {
-    page.style.backgroundColor = "lightgreen";   
-    page.style.color = "green";                  
-    page.style.border = "5px solid darkgreen";   
-    image.src = "https://greggvanourek.com/wp-content/uploads/2023/08/Nature-path-by-water-trees-and-mountains-AdobeStock_291242770-scaled.jpeg";
-    message.textContent = "green theme applied";
-} 
-else if (theme === "blue") {
-    page.style.backgroundColor = "lightblue";   
-    page.style.color = "blue";                  
-    page.style.border = "5px solid darkblue";   
-    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6HWRrkicvnDZ-RupPdWnyW3bMilY1JfDz8Q&s";
-    message.textContent = "blue theme applied";
-} 
-else if (theme === "red") {
-    page.style.backgroundColor = "pink";        
-    page.style.color = "red";                   
-    page.style.border = "5px solid darkred";    
-    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJrT7MI9fsrc6mWRBJBwhrf4vwTL7S5B8CzQ&s";
-    message.textContent = "red theme applied";
+    if (theme === "blue") {
+        theme.style.backgroundColor = "blue";
+        theme.style.color = darkblue;
+        img.src = "https://upload.wikimedia.org/wikipedia/commons/5/5e/Blue_ocean.jpg";
+        message.textContent = "theme blue applique";
+    }
+    if (theme === "red") {
+        theme.style.border = "red";
+        img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA2oASENe7GdCli1KnAl6cnDnuD6lGT60txQ&s";
+        message.textContent = "theme red applique";
+    } {
+        break; 
+    } else {
+        alert("Invalid input. Please type green, blue, or red"); 
+    }
 }
 
 if (theme === "green") {
-    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH8_j8hO5ekWfaRjYmYjZZ_qsfRzMRCbjXkw&s";  
-} 
-else if (theme === "blue") {
-    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTND3J7QUimP-6IFs4393f0Mvxg1cAE14tRrg&s";  // mets ici l'image que tu veux pour le thème bleu
-} 
-else if (theme === "red") {
-    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbspCVkfI_IJYPEpTu8Qbb9s-FE5sMbdNDbQ&s"; // mets ici l'image que tu veux pour le thème rouge
+    theme.border = "green";
+    theme.backgroundColor = "darkblue";
+    theme.color = "green";
+}
+if (theme === "blue") {
+    theme.border = "blue";
+    theme.backgroundColor = "blue";
+    theme.color = "blue";
+}
+if (theme === "red") {
+    theme.border = "red";
+    theme.border = "yellow";
+    theme.backgroundColor = "red";
+}
+if (theme === "green") {
+    message.textContent = "green theme applique";
+}
+if (theme === "blue") {
+    message.textContent = "blue theme applique";
+}
+if (theme === "red") {
+    message.textContent = "red theme applique";
 }
